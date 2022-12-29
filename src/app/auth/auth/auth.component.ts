efbaf9f666e93c53a9f6a6e98cb8f86b94c9ec6c
Travis Tidwell <travis@form.io>
2022-12-29 12:21:21 -0600

    Updated build.

--- src/app/auth/auth/auth.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { FormioAuthComponent } from '@formio/angular/auth';
-
-@Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
-})
-export class AuthComponent extends FormioAuthComponent {}
